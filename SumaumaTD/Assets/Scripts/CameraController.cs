41954f1aa276caaf0619070715878c868c71a982
Renan da Fonte <renandafonte@hotmail.com>
2017-01-24 16:54:21 -0200

    Camera Clamp (Bonus: Zoom Fixed)
    - Modificações no CameraController para criar variáveis para delimitar
    as posições máxima e mínima da câmera nos eixos x e z.
    - O zoom não é mais feito pelo eixo y mas agora pelo ortographicSize da
    câmera (talvez dê problemas se resolverem voltar a usar a perspectiva)
    - Foram criadas variáveis para definir o máximo e mínimo ortographic
    size (zoom) da câmera
    - Agora no Start(), a CameraController pega o componente da câmera e
    guarda na variável cam

--- SumaumaTD/Assets/Scripts/CameraController.cs
+++ SumaumaTD/Assets/Scripts/CameraController.cs
@@ -5,12 +5,28 @@ namespace Assets.Scripts
     public class CameraController : MonoBehaviour {
 
         ///TODO: Clamp side movements
-        public float MinY = 10f;
-        public float MaxY = 80f;
         public float ScrollSpeed = 5f;
         public float PanSpeed = 30f;
         public float PanBorderThickness = 10f;
-        
+
+        [Header("Camera Clamp")]
+        public float MinX = -40f;
+        public float MaxX = 70f;
+        public float MinZ = -80f;
+        public float MaxZ = 80f;
+
+        [Header("Zoom")]
+        [Tooltip("Quando menor o ortographic size, maior o zoom")] public float MinOrtographicSize = 5f;
+        [Tooltip("Quando menor o ortographic size, maior o zoom")] public float MaxOrtographicSize = 45f;
+
+        private Camera cam;
+
+        public void Start()
+        {
+            //Pega o componente da câmera (necessário para mudar o zoom -> ortographic size)
+            cam = this.GetComponent<Camera>();
+        }
+
         // Update is called once per frame
         public void Update ()
         {
@@ -21,24 +37,29 @@ namespace Assets.Scripts
                 return;
             }
 
+            Vector3 pos = transform.position;
+
             if (Input.GetKey ("w") || Input.mousePosition.y >= Screen.height - PanBorderThickness) {
-                transform.Translate (Vector3.forward * PanSpeed * Time.deltaTime, Space.World);
+                pos.z += PanSpeed * Time.deltaTime;
+                pos.z = Mathf.Clamp(pos.z, MinZ, MaxZ);
             }
             if (Input.GetKey ("s") || Input.mousePosition.y <= PanBorderThickness) {
-                transform.Translate (Vector3.back * PanSpeed * Time.deltaTime, Space.World);
+                pos.z -= PanSpeed * Time.deltaTime;
+                pos.z = Mathf.Clamp(pos.z, MinZ, MaxZ);
             }
             if (Input.GetKey ("d") || Input.mousePosition.x >= Screen.width - PanBorderThickness) {
-                transform.Translate (Vector3.right * PanSpeed * Time.deltaTime, Space.World);
+                pos.x += PanSpeed * Time.deltaTime;
+                pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
             }
             if (Input.GetKey ("a") || Input.mousePosition.x <= PanBorderThickness) {
-                transform.Translate (Vector3.left * PanSpeed * Time.deltaTime, Space.World);
+                pos.x -= PanSpeed * Time.deltaTime;
+                pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
             }
+            transform.position = pos;
 
             float scroll = Input.GetAxis ("Mouse ScrollWheel");
-            Vector3 pos = transform.position;
-            pos.y -= scroll * 1000 * ScrollSpeed * Time.deltaTime;
-            pos.y = Mathf.Clamp (pos.y, MinY, MaxY);
-            transform.position = pos;
+            cam.orthographicSize -= scroll * 1000 * ScrollSpeed * Time.deltaTime;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinOrtographicSize, MaxOrtographicSize);
         }
     }
 }
