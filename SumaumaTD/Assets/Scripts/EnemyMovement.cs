b2ad866d88b2662f4440271bc1ba4801eea8b672
Jose Eduardo Guimaraes Soares <je_soares94@yahoo.com.br>
2017-01-27 14:29:05 -0200

    Ze's mighty afternoon coding extravaganza Fix (feat. Renan)
    put everything on unity 5.3.0

--- SumaumaTD/Assets/Scripts/EnemyMovement.cs
+++ SumaumaTD/Assets/Scripts/EnemyMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
@@ -7,6 +8,7 @@ namespace Assets.Scripts
 
         private Transform _target;
         private int _wavepointIndex = 0;
+        private GameObject GameMaster; //setar o Game Master aqui
 
         private Enemy _enemy;
 
@@ -14,6 +16,7 @@ namespace Assets.Scripts
         {
             _enemy = GetComponent<Enemy>();
             _target = Waypoints.Points[0];
+            GameMaster = GameObject.Find("GameMaster");
         }
 
         public void Update()
@@ -46,6 +49,7 @@ namespace Assets.Scripts
         {
             WaveSpawner.EnemiesAlive--;
             PlayerStats.Lives--;
+            GameMaster.GetComponent<PlayerStats>().HealthBar.fillAmount = (float)PlayerStats.Lives / (float)PlayerStats.TotalLives; //pega a barra de vida do script PlayerStats e a diminui como no tutorial
             Destroy(gameObject);
         }
     }
--- SumaumaTD/Assets/Scripts/PlayerStats.cs
+++ SumaumaTD/Assets/Scripts/PlayerStats.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
@@ -10,9 +11,12 @@ namespace Assets.Scripts
         public static int Seeds;
         public int StartSeeds = 3;
         public static int Lives;
+        public static int TotalLives; //variavel criada para ser usada no script EnemyMovement
         public int StartLives = 20;
 
         public static int Waves;
+
+        [Header("UnityStuff")] public Image HealthBar;
 
         public void Start ()
         {
@@ -20,6 +24,7 @@ namespace Assets.Scripts
             //Money = StartMoney;
             Seeds = StartSeeds;
             Lives = StartLives;
+            TotalLives = StartLives; //start
         }
 
         public void Update ()
