46c4b8a35c8b5b392ca22a9cfe5766a5d44d4e57
Jose Eduardo Guimaraes Soares <je_soares94@yahoo.com.br>
2017-01-30 14:27:09 -0200

    Complete Global Saturation!
    The tree/base no changes sprites according to its lifebar and saturates
    with it as well

--- SumaumaTD/Assets/Scripts/EndUI.cs
+++ SumaumaTD/Assets/Scripts/EndUI.cs
@@ -5,6 +5,10 @@ namespace Assets.Scripts
 {
     public class EndUI : MonoBehaviour {
 
+        public Sprite treePlaceholderFullHealth;
+        public Sprite treePlaceholderMidHealth;
+        public Sprite treePlaceholderLowHealth;
+        public GameObject TreeSprites;
         public GameObject BaseColor;
         private Color _startColor;
 
@@ -13,26 +17,36 @@ namespace Assets.Scripts
 
         public void Start()
         {
-            _startColor = BaseColor.GetComponent<Renderer>().material.color;
+            _startColor = TreeSprites.GetComponent<SpriteRenderer>().color;
+            TreeSprites.GetComponent<SpriteRenderer>().sprite = treePlaceholderFullHealth;
+
         }
 
         public void Update()
         {
+            float h, s, v;
+
             HealthBar.fillAmount = (float)PlayerStats.Lives / (float)PlayerStats.TotalLives; //pega a barra de vida do script PlayerStats e a diminui como no tutorial
+
+            Color.RGBToHSV(_startColor, out h, out s, out v);
+
+            s = 1 - HealthBar.fillAmount;
+
+            TreeSprites.GetComponent<SpriteRenderer>().color = Color.HSVToRGB(h, s, v);
             
-            if (HealthBar.fillAmount <= 0.6f && HealthBar.fillAmount >= 0.3f)
+            if (HealthBar.fillAmount <= 0.6f && HealthBar.fillAmount >= 0.3f)//mid
             {
-                BaseColor.GetComponent<Renderer>().material.color = Color.blue;
+                TreeSprites.GetComponent<SpriteRenderer>().sprite = treePlaceholderMidHealth;
             }
 
-            else if (HealthBar.fillAmount < 0.3f)
+            else if (HealthBar.fillAmount < 0.3f)//low
             {
-                BaseColor.GetComponent<Renderer>().material.color = Color.grey;
+                TreeSprites.GetComponent<SpriteRenderer>().sprite = treePlaceholderLowHealth;
             }
 
-            else
+            else//full
             {
-                BaseColor.GetComponent<Renderer>().material.color = _startColor;
+                TreeSprites.GetComponent<SpriteRenderer>().sprite = treePlaceholderFullHealth;
             }
         }
     }
