3eecb0c75c74a43cef15be8cfa7a170bb922d27a
Renan da Fonte <renandafonte@hotmail.com>
2017-02-03 20:45:10 -0200

    Enemy machines now have sounds
    - Created a script (EnemySound) to handle the sounds for the enemy
    machines
    - Modifications on EnemyMovement to ask EnemySound to play a sound when
    enemies reach the end point
    - Modified the enemy prefab to include EnemySound and an AudioSource on
    it

--- SumaumaTD/Assets/Scripts/EnemyMovement.cs
+++ SumaumaTD/Assets/Scripts/EnemyMovement.cs
@@ -4,16 +4,19 @@ using UnityEngine.UI;
 namespace Assets.Scripts
 {
     [RequireComponent(typeof(Enemy))]
+    [RequireComponent(typeof(EnemySound))]
     public class EnemyMovement : MonoBehaviour {
 
         private Transform _target;
         private int _wavepointIndex = 0;
 
         private Enemy _enemy;
+        private EnemySound _sound;
 
         public void Start()
         {
             _enemy = GetComponent<Enemy>();
+            _sound = GetComponent<EnemySound>();
             _target = Waypoints.Points[0];
         }
 
@@ -47,6 +50,7 @@ namespace Assets.Scripts
         {
             WaveSpawner.EnemiesAlive--;
             PlayerStats.Lives--;
+            _sound.EnemyReachedEnd();
             Destroy(gameObject);
         }
     }
