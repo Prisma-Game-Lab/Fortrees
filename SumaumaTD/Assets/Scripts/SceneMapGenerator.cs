698d3e2e956a885be48bf3d0a82ef32d16f931f8
GiuDuncan <giuliaduncan@gmail.com>
2017-02-14 00:28:18 -0200

    Map Generator changes
    Como usar?
    veja o mapa 3, para indicar para qual waypoint o inimigo vai primeiro
    (se é da direita para esquerda ou vice versa) utilizar os sinais '>' e
    '<', respectivamente esquerda e direita, antes da linha em questao
    
    por exemplo, se a linha for:
    < NNPPPPNN
    o inimigo andará pelo P (path) no sentido:
    NN<<<<NN ( da direita para esquerda )

--- SumaumaTD/Assets/Scripts/SceneMapGenerator.cs
+++ SumaumaTD/Assets/Scripts/SceneMapGenerator.cs
@@ -1,16 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class SceneMapGenerator : MonoBehaviour
     {
         #region Variables   
+        
+        [Header("Mapa em txt")]
+        public TextAsset File;
+
+        [Header("Prefabs")]
         public GameObject NodesParentPrefab;
         public GameObject NodePrefab;
         public GameObject PathPrefab;
         public GameObject TurretUI;
-        public TextAsset File;
         public GameObject WaypointPrefab;
+        public GameObject StartPrefab;
+        public GameObject EndPrefab;
 
         [Header("Path sprites")]
         public Sprite PathCurveRD;
@@ -27,17 +35,27 @@ namespace Assets.Scripts
         private float _currentX;
         private float _currentZ;
         private bool _startNodeSet = false;
+        private WaypointsCoords _waypointsCoords;
+        private List<bool> _waypointDirections;
+
+        private const char StartLetter = 'S';
+        private const char EndLetter = 'E';
+        private const char PathLetter = 'P';
+        private const char NodeLetter = 'N';
         #endregion
 
         public void GenerateMap()
         {
             Debug.Log("Generating Map");
+            _waypointsCoords = new WaypointsCoords();
 
             GenerateGameObjectsParents();
 
             GenerateMapReadingsMatrix();
 
             BuildSceneMap();
+
+            SetAllWayPoints();
         }
 
         private void GenerateGameObjectsParents()
@@ -51,32 +69,67 @@ namespace Assets.Scripts
         private void GenerateMapReadingsMatrix()
         {
             var result = GetFileMapMatrixSizes();
-            _mapReadings = new char[result[0]][];
-            for (int l = 0; l < result[0]; l++)
+            _mapReadings = new char[result.J][];
+            for (int l = 0; l < result.J; l++)
             {
-                _mapReadings[l] = new char[result[1]];
+                _mapReadings[l] = new char[result.I[l]];
             }
 
             FillMapReadingsMatrix();
         }
 
-        private int[] GetFileMapMatrixSizes()
+        private Sizes GetFileMapMatrixSizes()
         {
-            int i = 0, j = 0;
-            var found = false;
+            int j = 0;
+            int[] i = Enumerable.Repeat(0, 100).ToArray(); ;
+            bool lineWasCreatedLastIteration = false;
             foreach (var letter in File.text)
             {
+                bool addWaypointDirection = false;
                 if (letter == '\n' || letter == '\r')
                 {
-                    found = true;
+                    if (lineWasCreatedLastIteration)
+                    {
+                        lineWasCreatedLastIteration = false;
+                        continue;
+                    }
                     j++;
+                    addWaypointDirection = true;
+                    lineWasCreatedLastIteration = true;
+                }
+                else if (letter == '<' || letter == '>')
+                {
+                    addWaypointDirection = true;
                 }
                 else
-                    if (!found)
-                    i++;
+                {
+                    if(!IsALetter(letter)) continue;
+                    lineWasCreatedLastIteration = false;
+                    i[j] = i[j] + 1;
+                }
+
+                if (addWaypointDirection)
+                {
+                    bool right = true;
+                    switch (letter)
+                    {
+                        case '<': //enemy moving left
+                            right = false;
+                            break;
+                        case '>': //enemy moving right
+                            right = true;
+                            break;
+                    }
+                    _waypointDirections.Add(right);
+                }
             }
             j++;
-            return new[] { j, i };
+            return new Sizes(j, i);
+        }
+
+        private bool IsALetter(char letter)
+        {
+            return letter == NodeLetter || letter == PathLetter || letter == StartLetter || letter == EndLetter;
         }
 
         private void FillMapReadingsMatrix()
@@ -85,7 +138,7 @@ namespace Assets.Scripts
             foreach (var letter in File.text)
             {
                 if (letter == '\r' || letter == '\n') continue;
-
+                if (!IsALetter(letter)) continue;
                 _mapReadings[i][j] = letter;
                 j++;
                 if (j >= _mapReadings[i].Length)
@@ -108,22 +161,41 @@ namespace Assets.Scripts
 
                     switch (letter)
                     {
-                        case 'N':
-                            GameObject temp = InstantiateNode();
-                            if (!_startNodeSet)
-                            {
-                                _nodesGameObject.StartNode = temp.GetComponent<Node>();
-                                _startNodeSet = true;
-                            }
+                        case NodeLetter:
+                            InstantiateNodeCase();
                             break;
-                        case 'P':
-                            InstantiatePath(x,y);
+                        case PathLetter:
+                            InstantiatePathCase(x,y);
                             break;
+                        case StartLetter:
+                            InstantiateNodeCase();
+                            InstantiateStartCase();
+                            break;
+                        case EndLetter:
+                            InstantiateNodeCase();
+                            InstantiateEndCase(y);
+                            break;
+                       default:
+                            _currentX -= 4.5f;
+                            break;
+
                     }
                     _currentX+=4.5f;
                 }
                 _currentX = 0;
                 _currentZ-=4.5f;
+            }
+        }
+
+        
+
+        private void InstantiateNodeCase()
+        {
+            GameObject temp = InstantiateNode();
+            if (!_startNodeSet)
+            {
+                _nodesGameObject.StartNode = temp.GetComponent<Node>();
+                _startNodeSet = true;
             }
         }
 
@@ -134,7 +206,7 @@ namespace Assets.Scripts
             return node;
         }
 
-        private void InstantiatePath(int i, int j)
+        private void InstantiatePathCase(int i, int j)
         {
             var path = (GameObject)Instantiate(PathPrefab, new Vector3(_currentX, 0, _currentZ), Quaternion.identity);
             path.transform.parent = _environmentGameObject.transform;
@@ -150,12 +222,12 @@ namespace Assets.Scripts
                 if (adjPath.Left)
                 {
                     SetSprite(path, PathCurveLU);
-                    SetWayPoint();
+                    SaveWayPoint(j);
                 }
                 else if (adjPath.Right)
                 {
                     SetSprite(path, PathCurveRU);
-                    SetWayPoint();
+                    SaveWayPoint(j);
                 }
                 else
                     SetSprite(path, PathVertical);
@@ -165,12 +237,12 @@ namespace Assets.Scripts
                 if (adjPath.Left)
                 {
                     SetSprite(path, PathCurveLD);
-                    SetWayPoint();
+                    SaveWayPoint(j);
                 }
                 else if (adjPath.Right)
                 {
                     SetSprite(path, PathCurveRD);
-                    SetWayPoint();
+                    SaveWayPoint(j);
                 }
                 else
                     SetSprite(path, PathVertical);
@@ -186,9 +258,33 @@ namespace Assets.Scripts
             path.GetComponentInChildren<SpriteRenderer>().sprite = sprite;
         }
 
-        private void SetWayPoint()
+        private void SaveWayPoint(int j)
         {
-            var waypoint = (GameObject)Instantiate(WaypointPrefab, new Vector3(_currentX, 0, _currentZ), Quaternion.identity);
+           _waypointsCoords.AddCoordinateToColumn(_currentX, _currentZ, j, _waypointDirections[j]);
+        }
+
+        private void SetAllWayPoints()
+        {
+            foreach (var coords in _waypointsCoords.Coordinates)
+            {
+                if(coords[0].Right)
+                    for (var j = 0; j < coords.Count; j++)
+                    {
+                        var current = coords[j];
+                        SetWayPoint(current.X, current.Z);
+                    }
+                else
+                    for (var j = coords.Count -1; j >= 0; j--)
+                    {
+                        var current = coords[j];
+                        SetWayPoint(current.X, current.Z);
+                    }
+            }
+        }
+
+        private void SetWayPoint(float x, float z)
+        {
+            var waypoint = (GameObject)Instantiate(WaypointPrefab, new Vector3(x, WaypointPrefab.transform.localPosition.y, z), Quaternion.identity);
             waypoint.transform.parent = _waypoints.transform;
         }
 
@@ -198,21 +294,35 @@ namespace Assets.Scripts
             adj.ResetAdjPaths();
             var coords = new [] {y - 1, x - 1, y + 1, x + 1};
             if (coords[0] >= 0)
-                if (_mapReadings[coords[0]][x] == 'P')
+                if (_mapReadings[coords[0]][x] == PathLetter || _mapReadings[coords[0]][x] == StartLetter || _mapReadings[coords[0]][x] == EndLetter)
                     adj.Up = true;
             if(coords[1] >= 0)
-                if (_mapReadings[y][coords[1]] == 'P')
+                if (_mapReadings[y][coords[1]] == PathLetter || _mapReadings[coords[0]][x] == StartLetter || _mapReadings[coords[0]][x] == EndLetter)
                     adj.Left = true;
             if (coords[2] < _mapReadings.Length)
-                if (_mapReadings[coords[2]][x] == 'P')
+                if (_mapReadings[coords[2]][x] == PathLetter || _mapReadings[coords[0]][x] == StartLetter || _mapReadings[coords[0]][x] == EndLetter)
                     adj.Down = true;
             if (coords[3] < _mapReadings[y].Length)
-                if (_mapReadings[y][coords[3]] == 'P')
+                if (_mapReadings[y][coords[3]] == PathLetter || _mapReadings[coords[0]][x] == StartLetter || _mapReadings[coords[0]][x] == EndLetter)
                     adj.Right = true;
 
             return adj;
         }
-        
+
+        private void InstantiateStartCase()
+        {
+            var start = (GameObject)Instantiate(StartPrefab, new Vector3(_currentX, StartPrefab.transform.localPosition.y, _currentZ), Quaternion.identity);
+
+            var waveSpawner = GameObject.FindObjectOfType<WaveSpawner>();
+            waveSpawner.SpawnPoint = start.transform;
+        }
+
+        private void InstantiateEndCase(int j)
+        {
+            SaveWayPoint(j);
+            var end = (GameObject)Instantiate(EndPrefab, new Vector3(_currentX, EndPrefab.transform.localPosition.y, _currentZ), Quaternion.identity);
+        }
+
         public struct AdjacentPath
         {
             public bool Up;
@@ -228,5 +338,19 @@ namespace Assets.Scripts
                 Right = false;
             }
         }
+
+        public struct Sizes
+        {
+            public int J;
+            public int[] I;
+
+            public Sizes(int j, int[] i)
+            {
+                J = j;
+                I = i;
+            }
+        }
+
+        
     }
 }
