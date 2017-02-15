019c2348ccf380907d3b78471616857e3bcac392
Jose Eduardo Guimaraes Soares <je_soares94@yahoo.com.br>
2017-02-15 14:49:03 -0200

    seed pls
    Implemented the heal base mechanic using the "HealBase.cs" script, I also put it on the "GameMaster.prefab"

---
+++ SumaumaTD/Assets/Scripts/HealBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class HealBase : MonoBehaviour
+{
+
+    private PlayerStats PlayerStats;
+
+    void Awake()
+    {
+        PlayerStats = GetComponent<PlayerStats>();
+    }
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () 
+    {
+	    if (Input.GetKeyDown("h") && PlayerStats.Seeds >= 5)
+	    {
+	        PlayerStats.Seeds = PlayerStats.Seeds - 5;
+            PlayerStats.Lives = PlayerStats.Lives + (int)Math.Round(0.2 * PlayerStats.StartLives, MidpointRounding.AwayFromZero); //arredondando pra cima, já que estamos lidando com ints
+            //PlayerStats.Lives = PlayerStats.Lives + (int)Math.Round(0.2 * PlayerStats.StartLives); //arredondando pra baixo
+	    }
+
+    }
+}
