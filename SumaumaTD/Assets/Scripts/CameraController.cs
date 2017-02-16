837e297a2bf94b3b0f5529dc9b30688d8c27f871
Renan da Fonte <renandafonte@hotmail.com>
2017-02-16 17:47:13 -0200

    May Rudá Guide Us II: Camera Clamp Fixed
    - Camera clamp now using a collider (that is a child of game master)
    - Removed CameraController from Main Scene's Camera (there's no need for
    it and it was causing errors)

--- SumaumaTD/Assets/Scripts/CameraController.cs
+++ SumaumaTD/Assets/Scripts/CameraController.cs
@@ -12,19 +12,10 @@ namespace Assets.Scripts
         public float ControllerScrollSpeed = 0.05f;
         public float PanSpeed = 30f;
         public float PanBorderThickness = 10f;
-        
+
         [Header("Camera Clamp")]
-		[Tooltip("Posição mínima em X da câmera quando o zoom está no mínimo")] public float MinXZoomMin = -40f;
-		[Tooltip("Posição máxima em X da câmera quando o zoom está no mínimo")] public float MaxXZoomMin = 70f;
-		[Tooltip("Posição mínima em Z da câmera quando o zoom está no mínimo")] public float MinZZoomMin = -80f;
-		[Tooltip("Posição máxima em Z da câmera quando o zoom está no mínimo")] public float MaxZZoomMin = 80f;
-
-		[Tooltip("Posição mínima em X da câmera quando o zoom está no máximo")] public float MinXZoomMax = -25f;
-		[Tooltip("Posição máxima em X da câmera quando o zoom está no máximo")] public float MaxXZoomMax = 55f;
-		[Tooltip("Posição mínima em Z da câmera quando o zoom está no máximo")] public float MinZZoomMax = -40f;
-		[Tooltip("Posição máxima em Z da câmera quando o zoom está no máximo")] public float MaxZZoomMax = 40f;
-
-        [Header("Zoom")]
+        public GameObject CameraClamp;
+        
         [Tooltip("Quando menor o ortographic size, maior o zoom")] public float MinOrtographicSize = 5f;
         [Tooltip("Quando menor o ortographic size, maior o zoom")] public float MaxOrtographicSize = 45f;
 
@@ -38,15 +29,19 @@ namespace Assets.Scripts
         public float DecreaseFactor = 1.0f;
 
         private Camera cam;
-		private float minX, minZ, maxX, maxZ;
+		//private float minX, minZ, maxX, maxZ;
         private float _shakeTimeRemaining;
+        private Transform _centerOfScreen;
+        private Collider _cameraClampCollider;
 
         public void Start()
         {
             //Pega o componente da câmera (necessário para mudar o zoom -> ortographic size)
             cam = this.GetComponent<Camera>();
 
-			UpdateClamp ();
+            _centerOfScreen = transform.GetChild(0);
+            
+            _cameraClampCollider = CameraClamp.GetComponent<Collider>();
         }
 
         // Update is called once per frame
@@ -62,38 +57,8 @@ namespace Assets.Scripts
             if(IsFixed)
                 return;
 
-            Vector3 pos = transform.position;
-			pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
-			pos.x = Mathf.Clamp(pos.x, minX, maxX);
-
-            if (Input.GetAxis("CameraVertical") > minimumAxisToMove || Input.GetKey ("w") || Input.mousePosition.y >= Screen.height - PanBorderThickness) {
-                pos.z += PanSpeed * Time.deltaTime;
-                //pos.z = Mathf.Clamp(pos.z, MinZ, MaxZ);
-            }
-            if (Input.GetAxis("CameraVertical") < -minimumAxisToMove || Input.GetKey ("s") || Input.mousePosition.y <= PanBorderThickness) {
-                pos.z -= PanSpeed * Time.deltaTime;
-                //pos.z = Mathf.Clamp(pos.z, MinZ, MaxZ);
-            }
-            if (Input.GetAxis("CameraHorizontal") > minimumAxisToMove || Input.GetKey ("d") || Input.mousePosition.x >= Screen.width - PanBorderThickness) {
-                pos.x += PanSpeed * Time.deltaTime;
-                //pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
-            }
-            if (Input.GetAxis("CameraHorizontal") < -minimumAxisToMove || Input.GetKey ("a") || Input.mousePosition.x <= PanBorderThickness) {
-                pos.x -= PanSpeed * Time.deltaTime;
-                //pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
-            }
-            transform.position = pos;
-
-            float controllerScroll = Input.GetAxis("CameraZoom");
-            float mouseScroll = Input.GetAxis ("Mouse ScrollWheel");
-       
-            float scroll = Mathf.Abs(controllerScroll) > Mathf.Abs(mouseScroll) ? controllerScroll : mouseScroll; //o maior valor entre controllerScroll e mouseScroll vira o valor de scroll
-            float scrollSpeed = (scroll == mouseScroll) ? MouseScrollSpeed : ControllerScrollSpeed;
-
-            cam.orthographicSize -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinOrtographicSize, MaxOrtographicSize);
-
-			UpdateClamp ();
+            Clamp();
+            //UpdateClamp ();
 
             CamShake();
         }
@@ -135,12 +100,69 @@ namespace Assets.Scripts
             }
         }
 
+        /*
 		private void UpdateClamp(){
 			float lerpAmount = 1 - (cam.orthographicSize - MinOrtographicSize) / (MaxOrtographicSize - MinOrtographicSize);
 			minX = Mathf.Lerp (MinXZoomMin, MinXZoomMax, lerpAmount); 
 			maxX = Mathf.Lerp (MaxXZoomMin, MaxXZoomMax, lerpAmount); 
 			minZ = Mathf.Lerp (MinZZoomMin, MinZZoomMax, lerpAmount); 
 			maxZ = Mathf.Lerp (MaxZZoomMin, MaxZZoomMax, lerpAmount); 
-		}
+		} */
+        
+        private void Clamp()
+        {
+            //Clamp antes do movimento
+            Vector3 pos = transform.position;
+
+            /*
+            pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            */
+            
+            //Movimento de câmera
+            if (Input.GetAxis("CameraVertical") > minimumAxisToMove || Input.GetKey("w") || Input.mousePosition.y >= Screen.height - PanBorderThickness)
+            {
+                pos.z += PanSpeed * Time.deltaTime;
+                //pos.z = Mathf.Clamp(pos.z, MinZ, MaxZ);
+            }
+            if (Input.GetAxis("CameraVertical") < -minimumAxisToMove || Input.GetKey("s") || Input.mousePosition.y <= PanBorderThickness)
+            {
+                pos.z -= PanSpeed * Time.deltaTime;
+                //pos.z = Mathf.Clamp(pos.z, MinZ, MaxZ);
+            }
+            if (Input.GetAxis("CameraHorizontal") > minimumAxisToMove || Input.GetKey("d") || Input.mousePosition.x >= Screen.width - PanBorderThickness)
+            {
+                pos.x += PanSpeed * Time.deltaTime;
+                //pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
+            }
+            if (Input.GetAxis("CameraHorizontal") < -minimumAxisToMove || Input.GetKey("a") || Input.mousePosition.x <= PanBorderThickness)
+            {
+                pos.x -= PanSpeed * Time.deltaTime;
+                //pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
+            }
+
+
+            //Clamp depois do movimento
+            transform.position = pos;
+            if (!_cameraClampCollider.bounds.Contains(_centerOfScreen.transform.position)) FixOutOfBounds();
+
+            //Zoom
+            float controllerScroll = Input.GetAxis("CameraZoom");
+            float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+
+            float scroll = Mathf.Abs(controllerScroll) > Mathf.Abs(mouseScroll) ? controllerScroll : mouseScroll; //o maior valor entre controllerScroll e mouseScroll vira o valor de scroll
+            float scrollSpeed = (scroll == mouseScroll) ? MouseScrollSpeed : ControllerScrollSpeed;
+
+            cam.orthographicSize -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinOrtographicSize, MaxOrtographicSize); 
+
+            
+        }
+        
+        private void FixOutOfBounds()
+        {
+            Vector3 nearestBound = _cameraClampCollider.ClosestPointOnBounds(_centerOfScreen.transform.position);
+            transform.Translate(nearestBound - _centerOfScreen.transform.position);
+        }
     }
 }
