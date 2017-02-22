d8069eef3c7c65d13614c37a8a338cfbaba67dc6
Renan da Fonte <renandafonte@hotmail.com>
2017-02-22 13:38:33 -0300

    Build fixes

--- SumaumaTD/Assets/Scripts/TurretBlueprint.cs
+++ SumaumaTD/Assets/Scripts/TurretBlueprint.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.Animations;
 
 namespace Assets.Scripts
 {
     [System.Serializable]
     public class TurretBlueprint
     {
-        public AnimatorController TurretAnimator;
+        public RuntimeAnimatorController TurretAnimator;
         public GameObject Prefab;
         public GameObject UpgradedPrefab;
         public int Cost;
