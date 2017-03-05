1bbd20336b26042b5864b01b2af9c4b2c52672e3
Renan da Fonte <renandafonte@hotmail.com>
2017-03-05 18:28:51 -0300

    Controller Fixes and Cursor no longer shown when a controller is connected
    - Created a script that hides the cursor if a controller is connected
    - Put the script on GameMaster prefab
    - Changed NodeDistance on Nodes prefab (and GDScene2 Nodes clone) to 4.5

---
+++ SumaumaTD/Assets/Scripts/HideCursorScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class HideCursorScript : MonoBehaviour
+    {
+        void Update()
+        {
+            if(Input.GetJoystickNames().Length > 0)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
