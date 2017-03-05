9c231aebfaf2b3594628801e754ef23b682ad0f7
GiuDuncan <giuliaduncan@gmail.com>
2017-03-05 17:11:04 -0300

    Heal and new trees counter

--- SumaumaTD/Assets/Scripts/HealBase.cs
+++ SumaumaTD/Assets/Scripts/HealBase.cs
@@ -1,31 +1,18 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
-using Assets.Scripts;
 
-public class HealBase : MonoBehaviour
+namespace Assets.Scripts
 {
-
-    private PlayerStats PlayerStats;
-
-    void Awake()
+    public class HealBase : MonoBehaviour
     {
-        PlayerStats = GetComponent<PlayerStats>();
-    }
-	
-	void Start () {
-	
-	}
-	
-	
-	void Update () 
-    {
-	    if (Input.GetKeyDown("h") && PlayerStats.Seeds >= 5)
-	    {
-	        PlayerStats.Seeds = PlayerStats.Seeds - 5;
-            PlayerStats.Lives = PlayerStats.Lives + (int)Math.Round(0.2 * PlayerStats.StartLives, MidpointRounding.AwayFromZero); //arredondando pra cima, já que estamos lidando com ints
-            //PlayerStats.Lives = PlayerStats.Lives + (int)Math.Round(0.2 * PlayerStats.StartLives); //arredondando pra baixo
-	    }
+        public void Update () 
+        {
+            if (Input.GetButtonDown("HealButton") && PlayerStats.Seeds >= 5 && PlayerStats.Lives < PlayerStats.TotalLives)
+            {
+                PlayerStats.Seeds = PlayerStats.Seeds - 5;
+                PlayerStats.Lives = PlayerStats.Lives + (int)Math.Round(0.2 * PlayerStats.TotalLives, MidpointRounding.AwayFromZero); //arredondando pra cima, já que estamos lidando com ints
+            }
 
+        }
     }
 }
---
+++ SumaumaTD/Assets/Scripts/ShowHealUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ShowHealUI : MonoBehaviour
+    {
+
+        public GameObject HealUI;
+
+        public void Start()
+        {
+            HealUI.SetActive(false);
+        }
+
+        public void Update () {
+		    if(PlayerStats.Lives < PlayerStats.TotalLives)
+                HealUI.SetActive(true);
+		    else
+		        HealUI.SetActive(false);
+        }
+    }
+}
